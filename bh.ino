2a288f36c387c0bbda71026fb476370650b489a9
EscA-phera <esca.phera@esphera.xyz>
2019-11-21 23:24:06 +0900

    Create bh.ino

---
+++ bh.ino
@@ -0,0 +1,79 @@
+# include <ESP8266.h>
+# include <ESP8266Client.h>
+# include <ArduinoJson.h>
+# include <SoftwareSerial.h>
+
+# include <Wire.h>
+# include <BH1750.h>
+
+# define SSID "jeong"
+# define PWD "0425433344"
+
+# define connServer "https://esphera.xyz"
+# define PORT "80"
+
+String PATH = "/api";
+
+SoftwareSerial esp = SoftwareSerial(11, 12);
+ESP8266 wifi = ESP8266(esp);
+
+BH1750 lightMeter;
+int LED = 3;
+int sensor = A0;
+
+StaticJsonBuffer<200> jsonBuffer;
+JsonObject& root = jsonBuffer.createObject();
+
+void setup()
+{ 
+  Serial.begin(115200);
+  wifi.begin();
+  wifi.joinAP(SSID, PWD);
+    
+  lightMeter.begin();
+  pinMode(LED, OUTPUT);
+  
+  Serial.println(getStatus(wifi.setMode(ESP8266_WIFI_STATION)));
+  Serial.println(getStatus(wifi.joinAP(SSID, PWD)));
+  Serial.println(getStatus(wifi.getAP(SSID)));
+    
+  Serial.println(wifi.read());
+}
+
+void loop()
+{
+  String data;
+  root["name"] = "ArduinoStatus";
+  root["data"] = "On";
+  root.printTo(data);
+
+  String postReq = "POST " + PATH + "HTTP/1.1\r\n"
+                   + "HOST " + connServer + "\r\n"
+                   + "ACCEPT: *" + "/" + "*\r\n"
+                   + "Content-Length: " + data.length() + "\r\n"
+                   + "Content-Type: application/json\r\n"
+                   + "\r\n" + data;
+  
+  uint16_t lux = lightMeter.readLightLevel();
+  
+  if ((lux <= 200) && (wifi.available()) && (data.length() > 0))
+  {
+    digitalWrite(LED, HIGH);
+    Serial.println(getStatus(wifi.send(postReq)));
+  }
+
+  else
+  {
+    digitalWrite(LED, LOW);
+  }
+}
+
+String getStatus(bool status)
+{
+  if (status == true);
+    return "OK";
+    
+  return "failed";
+}
+
+
